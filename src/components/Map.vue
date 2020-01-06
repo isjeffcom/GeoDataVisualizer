56088d3ffd953cd059f6feae883e0e15f119e644
JeffWu <isjeffcom@gmail.com>
2020-01-06 15:56:02 +0000

    Allows multiple datasets by user sets data. Preparing for editor. Add loading icon. Update map setting.

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -18,49 +18,53 @@
         :coordinates="MapCenter" 
         color="blue" 
       /-->
-
-      <MglMarker 
-        v-for="item in tempData" 
-        :key="item[0]" 
-        :coordinates="item.coor" 
-        color="blue" 
+      <div 
+        class="data-layer"
+        v-for="(ds, i) in datasets"
+        :key="ds.id"
       >
+        <div v-if="ds.marker.enable">
+          <MglMarker 
+            v-for="item in currentData[i]" 
+            :key="item[0]" 
+            :coordinates="item.coor" 
+            color="blue" 
+          >
 
-        <template slot="marker">
-          <div>
-            <img
-              src="../assets/plane.svg"
-              :ref="item[0]"
-              :style="'transform: rotate(' + item[10] + 'deg)'"
-              width="20"
-              height="20"
-              />
-          </div>
-        </template>
+            <template slot="marker">
+              <div v-if="ds.marker.type == 2">
+                <img
+                  :src="ds.marker.src"
+                  :ref="item[0]"
+                  :style="ds.marker.rotate ? 'transform: rotate(' + item[ds.marker.rotateSrc] + 'deg)' : ''"
+                  :width="ds.marker.size"
+                  />
+              </div>
+            </template>
 
-        <MglPopup
-          :closeButton="true"
-          :offset="[0, -20]">
-          <div>
-            <ul>
-              <li>ICAO Address: {{ item[0] }} </li>
-              <li>Callsign: {{ item[1] }} </li>
-              <li>On Ground: {{ item[8] ? "Yes" : "No" }} </li>
-              <li>Longitude: {{ item[5] }} </li>
-              <li>Latitude: {{ item[6] }} </li>
-              <li>Direction: {{ item[9] }} </li>
-              <li>Baro Altitude: {{ item[7] }} </li>
-              <li>Geo Altitude: {{ item[13] }} </li>
-              <li>Velocity: {{item[9]}} </li>
-              <li>Vertical Rate: {{ item[11] }} </li>
-              <li>Transponder Code: {{ item[14] }} </li>
-              <!--li>Position Update: {{ this.timeConverter(item[3]) }} </li>
-              <li>Last Contact: {{ this.timeConverter(item[4]) }} </li-->
-            </ul>
-          </div>
-        </MglPopup>
+            <div v-if="ds.popup.enable">
+              <MglPopup
+                :closeButton="true"
+                :offset="[0, -20]">
+                <div>
+                  <ul>
+                    <li 
+                      v-for="(ps, index) in ds.popup.display" 
+                      :key="index">
+                      
+                      {{ ds.struct.data[ps].displayName + ': ' + ds.struct.data[ps].front + item[ps] + ds.struct.data[ps].end}}
+
+                    </li>
+                  </ul>
+                </div>
+              </MglPopup>
+            </div>
+          
+          </MglMarker>
+        </div>
       
-      </MglMarker>
+      </div>
+      
 
       
 
@@ -70,7 +74,11 @@
 
 
     <div id="panel">
-      <button v-on:click="enable3D()">Enable 3D</button>
+      <button v-on:click="enable3D()">3D</button>
+    </div>
+
+    <div id="loading" v-if="isLoading">
+      <img src="../assets/loading.svg" alt="loading">
     </div>
   </div>
   
@@ -78,10 +86,10 @@
 </template>
 
 <script>
-import Mapbox from "mapbox-gl";
+//import Mapbox from "mapbox-gl";
 import { MglMap, MglMarker, MglPopup } from "vue-mapbox";
 import { genGet } from '../request'
-import { timeConverter } from '../utils'
+//import { timeConverter } from '../utils'
 
 import { CONF } from "../Conf"
 
@@ -93,7 +101,14 @@ export default {
   },
   data() {
     return {
+
+      // Data Sets
+      datasets: Array,
+      
       // Save For Global Access
+      updateIntervals: Array,
+      currentData: [],
+      isLoading: true,
       buildingLayerID: 0,
 
       // Mapbox Basic Global Conf
@@ -112,63 +127,26 @@ export default {
       MapBearing3D: -17.6,
 
       // Map Default Config
-      MapCenter: {lat: 55.952, lon: -3.188},
-      MapZoom: 15,
+      MapCenter: {lat: 51.5287718, lon: -0.2416819},
+      MapZoom: 7,
       MapSpeed: 1,
       MapPitch: this.Def_MapPitch,
       MapBearing: this.Def_Bearing,
       MapAntialias: false,
 
-      tempData:[],
       coor: [55.952, -3.188]
       
     };
-  },
-
-  watch: {
-    /*tempData: function () {
-      //this.map.resize();
-      this.tempData.forEach(el => {
-        const $ref = this.$refs[el[0]]
-        if($ref && $ref.length > 0){
-          $ref[0].style.transform = `rotate(${el[10]}deg)`
-        }
-        
-      });
-    },*/
   },
 
   created() {
 
     this.map = null
 
-    setInterval(()=>{
-      this.retrieveData({
-        api: "https://opensky-network.org/api/states/all",
-        parms:[
-          {
-            name: "lamin",
-            val: "49.458362"
-          },
-          {
-            name: "lamax",
-            val: "59.831400"
-          },
-          {
-            name: "lomin",
-            val: "-11.14"
-          },
-          {
-            name: "lomax",
-            val: "1.58"
-          }
-        ],
-        dataHead: "states"
-      })
-    }, 4000)
+    this.retrieveDataSets((datasets)=>{
+      this.parseDatasets(datasets)
+    })
 
-    
-    //console.log(this.mapbox.getCenter())
     
   },
   methods:{
@@ -176,22 +154,79 @@ export default {
 
       // in component
       this.map = event.map;
+      this.building3D()
       //this.$store.map = event.map;
 
     },
 
-    retrieveData(config) {
+    // Retrieve Datasets Config
+    retrieveDataSets(callback){
+      const that = this
+      const ds = []
+      genGet("./datasets.json", "", (res)=>{
+        if(res.status){
+          for(var i=0;i<res.data.length;i++){
+            if(res.data[i].enable){
+              ds.push(res.data[i])
+            }
+          }
+          that.datasets = ds
+          callback(ds)
+        }
+        
+      })
+    },
+
+    // Read Datasets
+    parseDatasets(datasets){
+      
+      var that = this
+
+      datasets.forEach((dataset, index) => {
+        
+        if(dataset.config.mode === "live"){
+          
+          if(dataset.source.type === "restful"){
+            
+            if(dataset.source.api.length > 0){
+              
+              if(dataset.source.keepUpdate){
+                setInterval(()=>{
+                  that.retrieveData({
+                    api: dataset.source.api,
+                    parms: dataset.source.parms,
+                    dataHead: dataset.source.dataHead
+                  }, index)
+                  
+                }, dataset.source.every * 1000)
+
+                //that.updateIntervals.$set(index, updating)
+              }
+              
+            } else {
+              console.log("Invaild API Request Format")
+            }
+          }
+        }
+      });
+    },
+
+    // Retrieve Live Data Feed
+    retrieveData(config, index) {
 
       /*config = {
         api: null,
         parms: [],
         dataHead: null
       }*/
-      if(config.api){
-        
-        genGet(config.api, config.parms, (result)=>{
 
-          var res
+      this.isLoading = true
+
+      if(config.api){
+
+        var res
+
+        genGet(config.api, config.parms, (result)=>{
 
           if(result.status){
             if(config.dataHead){
@@ -204,84 +239,115 @@ export default {
               res[i].coor = [res[i][5], res[i][6]]
             }
             
-
-            this.tempData = res
-
-            console.log("Data Retrieved")
+            if(this.currentData[index]){
+              this.$set(this.currentData,index, res)
+            } else {
+              this.currentData.push(res)
+            }
+            
+            //this.addToCData(res)
+            
+          } else {
+            console.log("Data Retrieve Fail: " + result.error)
           }
           
         })
+
+        this.isLoading = false
       }
     },
-    enable3D() {
 
+    enable3D(){
       if(!this.view3D){
         this.MapPitch = this.MapPitch3D
         this.MapBearing = this.MapBearing3D
-
-        // Insert the layer beneath any symbol layer.
-        var layers = this.map.getStyle().layers;
-        
-        for (var i = 0; i < layers.length; i++) {
-          if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-            this.buildingLayerID = layers[i].id;
-            break;
-          }
-        }
-
-        this.map.addLayer(
-          {
-            'id': '3d-buildings',
-            'source': 'composite',
-            'source-layer': 'building',
-            'filter': ['==', 'extrude', 'true'],
-            'type': 'fill-extrusion',
-            'minzoom': 15,
-            'paint': {
-              'fill-extrusion-color': '#aaa',
-              
-              // use an 'interpolate' expression to add a smooth transition effect to the
-              // buildings as the user zooms in
-              'fill-extrusion-height': [
-                'interpolate',
-                ['linear'],
-                ['zoom'],
-                15,
-                0,
-                15.05,
-                ['get', 'height']
-              ],
-              'fill-extrusion-base': [
-                'interpolate',
-                ['linear'],
-                ['zoom'],
-                15,
-                0,
-                15.05,
-                ['get', 'min_height']
-              ],
-              'fill-extrusion-opacity': 0.6
-            }
-          },
-          this.buildingLayerID
-        )
-        
-
-        
-
       } else {
         this.MapPitch = this.Def_MapPitch
         this.MapBearing = this.Def_Bearing
-        this.map.removeLayer(this.buildingLayerID)
+      }
+      this.view3D = !this.view3D
+    },
+
+    building3D() {
+
+      // Insert the layer beneath any symbol layer.
+      var layers = this.map.getStyle().layers;
+      
+      for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+          this.buildingLayerID = layers[i].id;
+          break;
+        }
       }
 
-      this.view3D = !this.view3D
+      this.map.addLayer(
+        {
+          'id': '3d-buildings',
+          'source': 'composite',
+          'source-layer': 'building',
+          'filter': ['==', 'extrude', 'true'],
+          'type': 'fill-extrusion',
+          'minzoom': 15,
+          'paint': {
+            'fill-extrusion-color': '#aaa',
+            
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            'fill-extrusion-height': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'height']
+            ],
+            'fill-extrusion-base': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'min_height']
+            ],
+            'fill-extrusion-opacity': 0.6
+          }
+        },
+        this.buildingLayerID
+      )
+    
+
+      
+    },
+
+    // Not working and cannot be async
+    replaceWording (source, val) {
+      var res = val
+      if(val == null || val == "" || !val){
+        return val
+      } else {
+        val = val.toString()
+      }
+
+      for(var i=0;i<source.length;i++){
+        if(val == source[i].from){
+          res = source[i].to
+        }
+      }
+      return res
     }
   }
 };
 </script>
 
 <style scoped>
+
+@keyframes spin {
+    from {transform:rotate(0deg);}
+    to {transform:rotate(360deg);}
+}
+
 #all{
   width: 100%;
   height: 100%;
@@ -289,7 +355,19 @@ export default {
 
 #panel{
   top: 10px;
-  right: 10px;
+  right: 20px;
   position: fixed;
+}
+
+#loading{
+  top: 10px;
+  left: 20px;
+  position: fixed;
+  z-index: 9999;
+}
+
+#loading img{
+  width: 20px;
+  animation: spin 1s infinite;
 }
 </style>
